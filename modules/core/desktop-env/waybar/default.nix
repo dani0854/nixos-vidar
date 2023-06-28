8fe08b8069d0aaceaf0a27122b3d2104a0e6c2ab
Danil Suetin <suetin085@protonmail.com>
2023-06-28 18:31:10 +0000

    Add waybar

---
+++ modules/core/desktop-env/waybar/default.nix
@@ -0,0 +1,10 @@
+{ pkgs, config, ... }:
+
+{
+  programs.waybar.enable = true;
+
+  home-manager.users.main = {
+    xdg.configFile."waybar/config".source = ./config;
+    xdg.configFile."waybar/style.css".source = ./style.css;
+  };
+}
