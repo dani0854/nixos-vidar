395755206717d104dca96c7d95bb1d6fc612c739
Danil Suetin <suetin085@protonmail.com>
2023-07-03 19:18:43 +0000

    Autostart cmst

--- modules/core/network/connman.nix
+++ modules/core/network/connman.nix
@@ -10,4 +10,20 @@
   environment.systemPackages = with pkgs; [
     cmst
   ];
+
+  systemd.user.services.cmst = {
+    enable = true;
+    description = "QT GUI for Connman with system tray icon";
+    partOf = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    after = [ "waybar.service" ];
+    serviceConfig = {
+        Type = "exec";
+        ExecStart = "${pkgs.cmst}/bin/cmst -m";
+        Slice = "session.slice";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+  };
 }
