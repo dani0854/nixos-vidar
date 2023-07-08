e79d06a4d89b2bbd2e93896ca780904852e29a28
Danil Suetin <suetin085@protonmail.com>
2023-07-08 15:12:53 +0700

    Add pavucontrol

--- modules/core/desktop-env/pipewire/default.nix
+++ modules/core/desktop-env/pipewire/default.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.pipewire.enable = true;
   services.pipewire.alsa.enable = true;
   services.pipewire.pulse.enable = true;
   services.pipewire.wireplumber.enable = true;
+  
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+  ];
 }
