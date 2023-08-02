8117366f98097635b6e849a2d1035b4b380a77f3
Danil Suetin <suetin085@protonmail.com>
2023-08-02 19:31:08 +0700

    Update

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  fonts.fonts = with pkgs; [
+  fonts.packages = with pkgs; [
       (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
       font-awesome
     ];
