d6dd7d3d2af4eeea2be21a86348177caa2f37d76
Danil Suetin <suetin085@protonmail.com>
2023-10-24 19:11:09 +0700

    Disable fprint auth if lid is closed

--- modules/core/doas.nix
+++ modules/core/doas.nix
@@ -14,6 +14,23 @@
     ];
   };
 
+  security.pam.services.doas.text = ''
+    # Account management.
+    account required pam_unix.so
+
+    # Authentication management.
+    auth sufficient pam_exec.so quiet stdout ${pkgs.bash}/bin/bash /run/current-system/sw/bin/fprintd-on-open-lid
+    auth sufficient pam_unix.so nullok likeauth try_first_pass
+    auth required pam_deny.so
+
+    # Password management.
+    password sufficient pam_unix.so nullok yescrypt
+
+    # Session management.
+    session required pam_env.so conffile=/etc/pam/environment readenv=0
+    session required pam_unix.so
+  '';
+
   environment.systemPackages = with pkgs; [
     doas-sudo-shim
   ];
--- modules/core/fprintd.nix
+++ modules/core/fprintd.nix
@@ -1,5 +1,27 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+	fprintdOnOpenLid = pkgs.writeScriptBin "fprintd-on-open-lid" ''
+		#!/usr/bin/env bash
+
+		if ${pkgs.gnugrep}/bin/grep -Fq closed /proc/acpi/button/lid/LID/state
+		then
+			echo "Lid is closed, skipping fingerprint auth"
+			exit 1
+		else
+			echo "Place your right index finger on the fingerprint reader"
+			${pkgs.fprintd}/bin/fprintd-verify > /dev/null
+		fi
+	'';
+in
 {
-	services.fprintd.enable = true;
+	environment.systemPackages = [
+		pkgs.fprintd
+		fprintdOnOpenLid
+	];
+	
+  systemd.packages = [ pkgs.fprintd ];
+
+	services.dbus.packages = [ pkgs.fprintd ];
+
 }
