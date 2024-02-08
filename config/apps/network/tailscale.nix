62b1325c57618cff1cb78290734f541c5a711e24
Danil Suetin <suetin085@protonmail.com>
2024-02-08 19:23:46 +0700

    Add tailscale

--- config/apps/network/tailscale.nix
+++ config/apps/network/tailscale.nix
@@ -1,3 +1,13 @@
 { ... }: {
-  services.tailscale.enable = false;
+  services.tailscale = {
+    enable = true;
+    extraUpFlags = [ "--accept-dns=false" ];
+  };
+
+  services.unbound.settings.forward-zone = [
+    {
+      name = "ts.net.";
+      forward-addr = "100.100.100.100";
+    }
+  ];
 }
