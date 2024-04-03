7ca75ac70b3b98fe87c9898e575d548a668192ae
Danil Suetin <suetin085@protonmail.com>
2024-04-04 01:22:49 +0500

    Remove wezterm (bad wayland support)

--- config/core/desktop-env/wezterm/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    wezterm
-  ];
-
-  home-manager.users.main = {
-    xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
-  };
-}
