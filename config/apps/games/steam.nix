392f989100dd58dbbe437a36179353ad0c387bf6
Danil Suetin <suetin085@protonmail.com>
2024-05-08 01:48:41 +0500

    steam: add proton-ge and some utils

--- config/apps/games/steam.nix
+++ config/apps/games/steam.nix
@@ -1,8 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.steam = {
     enable = true;
-    gamescopeSession.enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    gamemode
+  ];
 
   hardware.steam-hardware.enable = true;
 }
