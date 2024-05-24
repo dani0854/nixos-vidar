64ed081cb0028d050e3b81ec53b4b186773c85fb
Danil Suetin <suetin085@protonmail.com>
2024-05-24 23:40:05 +0500

    mpv: disable scroll

--- config/apps/media/mpv.nix
+++ config/apps/media/mpv.nix
@@ -1,5 +1,11 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    mpv
-  ];
+{ ... }: {
+  home-manager.users.main.programs.mpv = {
+    enable = true;
+    bindings = {
+      WHEEL_UP = "ignore";
+      WHEEL_DOWN = "ignore";
+      WHEEL_RIGHT = "ignore";
+      WHEEL_LEFT = "ignore";
+    };
+  };
 }
