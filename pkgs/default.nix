2a5203ffb888451b7198f9691fec37a7ffb0a2b4
Danil Suetin <suetin085@protonmail.com>
2024-12-22 19:33:25 +0100

    sparrow-wifi: add

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,4 @@
 { pkgs, ... }:
-rec {
-
-  # gns3Packages = pkgs.dontRecurseIntoAttrs (pkgs.callPackage ./gns3 { });
-  # gns3-gui = gns3Packages.guiStable;
-  # gns3-server = gns3Packages.serverStable;
+{
+  sparrow-wifi = pkgs.callPackage ./sparrow-wifi { };
 }
