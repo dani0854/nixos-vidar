146688dc77d7456d981ed77192e487a0f444ae76
Danil Suetin <suetin085@protonmail.com>
2025-03-29 22:52:26 +0100

    tmux: remove sensible config

--- config/core/tmux.nix
+++ config/core/tmux.nix
@@ -9,6 +9,7 @@
     package = pkgs.tmux;
     keyMode = "vi";
     escapeTime = 5;
+    clock24 = true;
     terminal = "tmux-256color";
     extraConfig = "set -as terminal-features 'alacritty:rgb'\n";
     plugins = with pkgs; [
