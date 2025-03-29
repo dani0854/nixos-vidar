e21237dc3c62bb99b6da8e58552fde899d77529e
Danil Suetin <suetin085@protonmail.com>
2025-03-29 20:51:51 +0100

    core: add nh

---
+++ config/core/nh.nix
@@ -0,0 +1,11 @@
+{ config, ... }:
+{
+  programs.nh = {
+    enable = true;
+    clean = {
+      enable = true;
+      extraArgs = "--keep-since 4d --keep 3";
+    };
+    flake = "${config.users.users.main.home}/nixos-vidar";
+  };
+}
