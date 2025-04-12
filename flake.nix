14895c226910c633128c68a58ba5966b8313f655
Danil Suetin <suetin085@protonmail.com>
2025-04-12 13:26:40 +0200

    update

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.systems.follows = "systems";
     };
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
