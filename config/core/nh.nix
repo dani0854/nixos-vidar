2245e19cd103291da9b0f905e01fc679d1e995f3
Danil Suetin <suetin085@protonmail.com>
2025-04-12 13:27:43 +0200

    nh: fix system config path

--- config/core/nh.nix
+++ config/core/nh.nix
@@ -6,6 +6,6 @@
       enable = true;
       extraArgs = "--keep-since 4d --keep 3";
     };
-    flake = "${config.users.users.main.home}/nixos-vidar";
+    flake = "${config.users.users.main.home}/nixos-vidar/";
   };
 }
