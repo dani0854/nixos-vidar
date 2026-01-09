fc463cd06e070e2a4169296f20a22ca0ba729ddc
Danil Suetin <suetin085@proton.me>
2026-01-09 21:34:08 +0100

    gpg: add config

--- config/apps/security/gnupg.nix
+++ config/apps/security/gnupg.nix
@@ -1,4 +1,13 @@
 { ... }:
 {
   programs.gnupg.agent.enable = true;
+
+  home-manager.users.main.programs.gpg = {
+    enable = true;
+    settings = {
+      default-new-key-algo = "ed25519/cert+ed25519/sign";
+      auto-key-retrieve = true;
+      keyserver-options = "honor-keyserver-url";
+    };
+  };
 }
